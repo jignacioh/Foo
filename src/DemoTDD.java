dbe96e9b368f580125ab351116f7deb16b577156
jhurtace <juanhurtado0405@gmail.com>
2017-08-04 15:39:47 -0500

    first commit

---
+++ src/DemoTDD.java
@@ -0,0 +1,15 @@
+
+public class DemoTDD {
+
+	private static final String TEXT_STATIC = "ABDE";
+
+	public static void main(String[] args) {
+		
+		String name="abde";
+		if(name.equalsIgnoreCase(TEXT_STATIC)) {
+			
+			
+		}
+	}
+
+}
